ec941f7c42317704ab9e278f28976984aa51cc1c
Usama Hassan <139792550+MuhammadUsama-Hassan@users.noreply.github.com>
2023-11-10 22:24:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let adda = document.querySelector(".btn-dark");
+
+adda.addEventListener("click", ()=>{
+    document.querySelector("body").style.backgroundColor="black"
+    document.querySelector("body").style.color="white" 
+} )
+
+let addali = document.querySelector(".btn-light");
+
+addali.addEventListener("click", ()=>{
+    document.querySelector("body").style.backgroundColor= "white";
+    document.querySelector("body").style.color= "black";
+    
+})
